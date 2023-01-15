5ec2422e1634dc872c1eb7ac6bc9947df376350d
deartis <deartissolu@gmail.com>
2023-01-15 19:46:47 -0300

    Tela de login

--- lib/widgets/form_login.dart
+++ lib/widgets/form_login.dart
@@ -10,60 +10,98 @@ class FormLogin extends StatefulWidget {
 class _FormLoginState extends State<FormLogin> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Icon(
-          Icons.person,
-          color: Color(0xFFFFFFFF),
-          size: 100.0,
-        ),
-        TextField(
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            label: const Text('Email'),
-            labelStyle: const TextStyle(
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Image(image: AssetImage("lib/assets/img/logo.png")),
+            const Text(
+              'JD REFEIÇÕES',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+
+            /*const Icon(
+              Icons.person,
               color: Color(0xFFFFFFFF),
+              size: 100.0,
+            ),*/
+            const SizedBox(height: 100),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(
+                  Icons.email,
+                  color: Color(0xFF4D0303),
+                ),
+                filled: true,
+                fillColor: const Color.fromRGBO(255, 255, 255, 0.3),
+                label: const Text('Email'),
+                labelStyle: const TextStyle(
+                  color: Color(0xFFFFFFFF),
+                  fontSize: 20,
+                ),
+                hintText: 'exempl@exemplo.com',
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                  borderSide: const BorderSide(color: Color(0xFFFFFFFF)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                  borderSide: const BorderSide(color: Color(0xFFFFFFFF)),
+                ),
+              ),
             ),
-            hintText: 'exempl@exemplo.com',
-            hintStyle: const TextStyle(
-              color: Colors.grey,
+            const SizedBox(height: 20),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(
+                  Icons.key,
+                  color: Color(0xFF4D0303),
+                ),
+                filled: true,
+                fillColor: const Color.fromRGBO(255, 255, 255, 0.3),
+                label: const Text('Senha'),
+                labelStyle: const TextStyle(
+                  color: Color(0xFFFFFFFF),
+                  fontSize: 20,
+                ),
+                hintText: '****',
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                  borderSide: const BorderSide(color: Color(0xFFFFFFFF)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                  borderSide: const BorderSide(color: Color(0xFFFFFFFF)),
+                ),
+              ),
             ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(6),
-              borderSide: const BorderSide(color: Color(0xFFFFFFFF)),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFFFAF00),
+                minimumSize: const Size.fromHeight(50),
+              ),
+              child: const Text(
+                'Entrar',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(6),
-              borderSide: const BorderSide(color: Color(0xFFFFFFFF)),
-            ),
-          ),
+          ],
         ),
-        const SizedBox(height: 20),
-        TextField(
-          obscureText: true,
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            label: const Text('Senha'),
-            labelStyle: const TextStyle(
-              color: Color(0xFFFFFFFF),
-            ),
-            hintText: '****',
-            hintStyle: const TextStyle(
-              color: Colors.grey,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(6),
-              borderSide: const BorderSide(color: Color(0xFFFFFFFF)),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(6),
-              borderSide: const BorderSide(color: Color(0xFFFFFFFF)),
-            ),
-          ),
-        ),
-        const SizedBox(height: 20),
-        ElevatedButton(onPressed: (){}, child: const Text('Entrar')),''
-      ],
+      ),
     );
   }
 }
